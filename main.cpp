1cb22ed1033802609a6f4fd4da8fadcef2f4b80f
ManonVessiot <manon.vessiot@hotmail.com>
2020-10-03 15:37:51 +0200

    Add load calibration

--- main.cpp
+++ main.cpp
@@ -15,13 +15,17 @@ void display()
     runningLock.lock(); // Thread Lock
     cam.running = true;
     runningLock.unlock(); // Thread unLock
-    while (true)
+    bool stop = false;
+    while (!stop)
     {
         frameLock.lock(); // Thread Lock
         cam.updateFrame();
         cv::imshow(cam.getWindowsName(), cam.getFrame());
         frameLock.unlock(); // Thread unLock
         
+        runningLock.lock(); // Thread Lock
+        stop = !cam.running;
+        runningLock.unlock(); // Thread unLock
         if (waitKey(5) >= 0){
             break;
         }
@@ -54,18 +58,77 @@ void recordFrameForCalibrationProcess(int numberOfInput, int secondsBetweenFrame
             break;
         }
     }
-    cout << calib.inputSize() << " input recorded / " << numberOfInput << " : DONE" << endl;
-    
+    runningLock.lock(); // Thread Lock
+    cam.running = false;
+    runningLock.unlock(); // Thread unLock   
+}
+
+bool saveCalibration(int numberOfInput, int secondsBetweenFrame, string calibrationFile){
+    recordFrameForCalibrationProcess(numberOfInput, secondsBetweenFrame);
+
+    if (calib.inputSize() == numberOfInput){
+        calib.calibrate(cam.cameraMatrix, cam.distanceCoefficients);
+        calib.saveCalibration(calibrationFile);
+        return true;
+    }
+    return false;
+}
+
+bool loadCalibration(string calibrationFile){
+    ifstream inStream(calibrationFile);
+    if (inStream){
+        uint16_t rows;
+        uint16_t columns;
+
+        // cameraMatrix
+        inStream >> rows;
+        inStream >> columns;
+
+        cam.cameraMatrix = Mat(Size(columns, rows), CV_64F);
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < columns; c++)
+            {
+                double read = 0.0f;
+                inStream >> read;
+                cam.cameraMatrix.at<double>(r, c) = read;
+                cout << cam.cameraMatrix.at<double>(r, c) << endl;
+            }            
+        }
+
+        // distanceCoefficients
+        inStream >> rows;
+        inStream >> columns;
+
+        cam.distanceCoefficients = Mat::zeros(columns, rows, CV_64F);
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < columns; c++)
+            {
+                double read = 0.0f;
+                inStream >> read;
+                cam.distanceCoefficients.at<double>(r, c) = read;
+                cout << cam.distanceCoefficients.at<double>(r, c) << endl;
+            }            
+        }
+        inStream.close();
+
+        return true;
+    }
+    return false;
 }
 
 int main( int argc, char **argv ){
-    // ./exc 0 9 6 0.025 0 30 1 calibration.txt Webcam
-    if (argc < 9){
-        cout << "./exc capIndex boardSize_Width boardSize_Height square_Size calibrate_Pattern numberOfInput secondsBetweenFrame calibrationFileName (windowsName)" << endl;
+    // ./exc 0 0 9 6 0.025 0 30 1 calibration.txt Webcam
+    if (argc < 10){
+        cout << "./exc mode capIndex boardSize_Width boardSize_Height square_Size calibrate_Pattern numberOfInput secondsBetweenFrame calibrationFileName (windowsName)" << endl;
+        cout << " --> mode :" << endl;
+        cout << "      -> Load Calibration = 0" << endl;
+        cout << "      -> Save Calibration = 1" << endl;
         cout << " --> capIndex identify your webcam" << endl;
         cout << " --> boardSize_Width, boardSize_Height is the size of your pattern" << endl;
         cout << " --> square_Size is the size of your pattern's square" << endl;
-        cout << " --> calibrate_Pattern is the type of pattern" << endl;
+        cout << " --> calibrate_Pattern is the type of pattern :" << endl;
         cout << "      -> CHESSBOARD = 0" << endl;
         cout << "      -> CIRCLES_GRID = 1" << endl;
         cout << "      -> ASYMMETRIC_CIRCLES_GRID = 2" << endl;
@@ -77,43 +140,51 @@ int main( int argc, char **argv ){
         return -1;
     }
 
-    int capIndex = atoi(argv[1]); // 0
-    int boardSize_Width = atoi(argv[2]); // 9 or 6 or 11
-    int boardSize_Height = atoi(argv[3]); // 6 or 6 or 4
-    float square_Size = atof(argv[4]); // 0.025
-    Pattern calibrate_Pattern = (Pattern) atoi(argv[5]); // CHESSBOARD = 0 or CIRCLES_GRID = 1 or ASYMMETRIC_CIRCLES_GRID = 2
-    int numberOfInput = atoi(argv[6]); // 30
-    int secondsBetweenFrame = atoi(argv[7]); // 1
+    // variables
+    int mode = atoi(argv[1]);
+    int capIndex = atoi(argv[2]); // 0
+    int boardSize_Width = atoi(argv[3]); // 9 or 6 or 11
+    int boardSize_Height = atoi(argv[4]); // 6 or 6 or 4
+    float square_Size = atof(argv[5]); // 0.025
+    Pattern calibrate_Pattern = (Pattern) atoi(argv[6]); // CHESSBOARD = 0 or CIRCLES_GRID = 1 or ASYMMETRIC_CIRCLES_GRID = 2
+    int numberOfInput = atoi(argv[7]); // 30
+    int secondsBetweenFrame = atoi(argv[8]); // 1
 
-    string calibrationFile = argv[8]; // "calibration.txt"
+    string calibrationFile = argv[9]; // "calibration.txt"
 
     string windowsName = "Live";
-    if (argc >=10){
-        windowsName = argv[9];
+    if (argc >=11){
+        windowsName = argv[10];
     }
-
-
     VideoCapture cap(capIndex);
     cam = Video(cap, windowsName);
     calib = CalibrationProcess(boardSize_Width, boardSize_Height, square_Size, calibrate_Pattern);
 
+
     // init for thread
     XInitThreads();
 
-    cout << "start display" << endl;
+
+    // start display video capture
+    cout << "start display..." << endl;
     thread displayThread(display);
-    cout << "start record" << endl;
-    thread recordframeLockead(recordFrameForCalibrationProcess, numberOfInput, secondsBetweenFrame);
 
-    recordframeLockead.join();
-    cout << "stop record" << endl;
 
-    if (calib.inputSize() == numberOfInput){
-        calib.calibrate(cam.cameraMatrix, cam.distanceCoefficients);
-        calib.saveCalibration(calibrationFile);
+    // calibration
+    thread calibrationThread;
+    
+    if (mode == 0){
+        cout << "loading calibration..." << endl;
+        calibrationThread = thread(loadCalibration, calibrationFile);
     }
+    else if (mode == 1){
+        cout << "saving calibration..." << endl;
+        calibrationThread = thread(saveCalibration, numberOfInput, secondsBetweenFrame, calibrationFile);
+    }
+    
+
+    calibrationThread.join();
     displayThread.join();
-    cout << "stop display" << endl;
 
     return 0;
 }
